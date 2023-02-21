504fb4770cb8e1c6ef8d1f40ace276cf3594a418
8bitaby <abyvjab@gmail.com>
2023-02-21 23:10:18 +0530

    homeView and SearchView

--- djangodictionary/dictionary/views.py
+++ djangodictionary/dictionary/views.py
@@ -1,3 +1,11 @@
+# importing the render function from django.shortcuts
+# the render function renders templates
 from django.shortcuts import render
 
-# Create your views here.
+# this is the view that will render the index page
+def homeView(request):
+    return render(request, 'dictionary/index.html')
+
+# this is the view that will render search page
+def searchView(request):
+    return render(request, 'dictionary/search.html')
